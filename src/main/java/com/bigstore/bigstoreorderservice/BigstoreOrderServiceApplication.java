f2eee97f229c89ac0c52a5400744cfeb1ddcd451
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 17:49:56 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstoreorderservice/BigstoreOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoreorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreOrderServiceApplication.class, args);
+	}
+
+}
